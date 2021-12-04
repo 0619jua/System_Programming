5136ad8608cd3c8b17e091fe94bb971b47b0656c
0619jua <84326090+0619jua@users.noreply.github.com>
2021-12-04 21:04:43 +0900

    Update client1.c

--- Socket_Programming/client1.c
+++ Socket_Programming/client1.c
@@ -1,58 +1,75 @@
-/*client1*/
+/*c2*/
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/un.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/un.h>
 
-#define PORT 8062
+#define SOCKET_NAME "hs1"
 #define QUITMSG "exit"
 
 int main(void){
 	char buf[256];
-	struct sockaddr_in server;
-	int sd;
+	struct sockaddr_un server, client;
+	int s1, len;
 
-	if((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+	if((s1 = socket(AF_UNIX, SOCK_STREAM, 0)) == -1){
 		perror("socket");
 		exit(1);
 	}
+	
+	memset((char *)&server, 0, sizeof(struct sockaddr_un));
+	server.sun_family = AF_UNIX;
+	strcpy(server.sun_path, SOCKET_NAME);
+	len = sizeof(server.sun_family) + strlen(server.sun_path);
 
-	memset((char *)&server, '\0', sizeof(server));
-	server.sin_family = AF_INET;
-	server.sin_port = htons(PORT);
-	server.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	if(connect(sd, (struct sockaddr *)&server, sizeof(server))){
+	if(connect(s1, (struct sockaddr *)&server, len) <0){
 		perror("connect");
 		exit(1);
 	}
-//	char result = '';
+
+
+	//menu recv
+	if(recv(s1, buf, sizeof(buf), 0) > 0){
+		printf("%s\n", buf);
+	}
+
+	printf("Choose the menu : ");
+	gets(buf);
+	if(send(s1, buf, strlen(buf)+1, 0) == -1){
+		perror("send");
+		exit(1);
+	}	
+
+	memset(buf, 0x00, sizeof(buf));
+	if(recv(s1, buf, sizeof(buf)+1, 0) > 0 ){
+		printf("%s\n", buf);
+	}
+
+
 	while(1){
-		
 		memset(buf, 0x00, sizeof(buf));
-		if(recv(sd, buf, sizeof(buf), 0) >0){
-			printf("[RECV] : %s\n", buf);
-		}
- 		
-		memset(buf, 0x00, sizeof(buf));
+
 		printf("[SEND_C1] : ");
 		gets(buf);
-		strcat("[SEND_C1]", buf);
+		strcat(buf, " [SEND_C1] ");
 
-		if(send(sd, buf, strlen(buf) +1, 0) == -1){
+		if(send(s1, buf, strlen(buf) +1, 0) == -1){
 			perror("send");
 			exit(1);
 		}
-
+		
 		if(strncmp(buf, QUITMSG, strlen(QUITMSG)) == 0){
 			exit(1);
-		}
-			
-	}//while
+		}	
 
-	close(sd);
+	}
+
+	close(s1);
 	return 0;
+	
 }
