7ecb3e8f08ba9aba12729ad0816529c20ba7b44c
0619jua <84326090+0619jua@users.noreply.github.com>
2021-12-04 21:05:26 +0900

    Update server1.c

--- Socket_Programming/server1.c
+++ Socket_Programming/server1.c
@@ -1,93 +1,141 @@
-/*server1*/
+/*s1*/
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/un.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#define PORT 8062
+#define SOCKET_NAME "hs1"
+#define PORT 9001
 #define QUITMSG "exit"
 #define INFO "<Choose menu>\n1. ChatRoom\n2. HangMan Game\n3. Game Score"
 
-int main(void){
-	char buf[256];
-	struct sockaddr_in server, client;
-	int s1, s2, clientlen = sizeof(client);
 
+int main(void){
+	//INET
+	char buf[256];
+	struct sockaddr_in server;
+	int s1;
+
+	//UNIX
+	char buf2[256];
+	struct sockaddr_un server2, client2;
+	int s3, s4, len, clen;
+
+
+	//INET
 	if((s1 = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 		perror("socket");
 		exit(1);
 	}
 
+	//UNIX
+	if((s3 = socket(AF_UNIX, SOCK_STREAM, 0)) == -1){
+		perror("socket");
+		exit(1);
+	}
+
+	//INET
 	memset((char *)&server, '\0', sizeof(server));
 	server.sin_family = AF_INET;
 	server.sin_port = htons(PORT);
 	server.sin_addr.s_addr = inet_addr("127.0.0.1");
-	
-	if(bind(s1, (struct sockaddr *)&server, sizeof(server))) {
+
+	//UNIX
+	memset((char *)&server2, 0, sizeof(struct sockaddr_un));
+	server2.sun_family = AF_UNIX;
+	strcpy(server2.sun_path, SOCKET_NAME);
+	len = sizeof(server2.sun_family) + strlen(server2.sun_path);
+
+
+	//UNIX
+	if(bind(s3, (struct sockaddr *)&server2, len)){
 		perror("bind");
 		exit(1);
 	}
-	
-	if(listen(s1, 5)){
+
+	if(listen(s3, 5) <0){
 		perror("listen");
 		exit(1);
 	}
 
-	
-		
-	while(1){
 
-		if((s2 = accept(s1, (struct sockaddr *)&client, &clientlen)) == -1){
+	while(1){
+	
+		if((s4 = accept(s3, (struct sockaddr *)&client2, &clen)) == -1){
 			perror("accept");
 			exit(1);
 		}
-		
-		//send info(menu) 
-		if(send(s2, INFO, strlen(INFO) +1, 0)==-1) {
-			perror("send");
+	
+		printf("New Client!\n");
+	
+		//INET
+		if(connect(s1, (struct sockaddr *)&server, sizeof(server))){
+			perror("connect");
 			exit(1);
 		}
-		
-		//receive menu number 
+
+
 		memset(buf, 0x00, sizeof(buf));
-		if(recv(s2, buf, sizeof(buf), 0) >0 ) {
-			printf("[RECV] : %s\n");	
+		if(send(s4, INFO, strlen(INFO)+1, 0) == -1 ){
+			perror("send");
+			exit(1);
+		}	
+
+		memset(buf2, 0x00, sizeof(buf2));
+		if(recv(s4, buf2, sizeof(buf2), 0) == -1){
+			perror("recv");
+			exit(1);
 		}
 
-
-		//menu
-		switch(atoi(buf)){
-			case 1 :	//chat room
-				strcpy(buf, "Welcome to Chat Room");
-				if(send(s2, buf, strlen(buf)+1, 0) == -1){
+		switch(atoi(buf2)){
+		case 1 : //chat room
+				printf("\nWelcome to Chat Room!!\n");
+				memset(buf2, 0x00, sizeof(buf2));
+				if(send(s4, "--Welcome to Chat Room--", 256, 0) == -1){
 					perror("send");
 					exit(1);
 				}
-
-				while(recv(s2, buf, 256, 0) > 0){
-					printf("[RECV] : %s\n", buf);
-					send(s2, buf, 256, 0); 
-					memset(buf, 0x00, sizeof(buf));
-					
-					if(strncmp(buf, QUITMSG, strlen(QUITMSG)) == 0) {
-						printf("[Info] Client quit!\n");
-						break;
+			
+				while(1){
+	
+					memset(buf2, 0x00, sizeof(buf2));
+					if(recv(s4, buf2, sizeof(buf2), 0)  > 0){
+						printf("[C1] : %s\n", buf2);
+//						send(s1, buf2, strlen(buf2)+1, 0);
 					}
+						
+					if(send(s1, buf2, strlen(buf2)+1, 0) == -1){
+  	                  perror("send");
+  	  	               exit(1);
+  	   	      }
+	
 					memset(buf, 0x00, sizeof(buf));
-				}
-
+					if(recv(s1, buf, sizeof(buf), 0) > 0){
+						printf("[C2] : %s\n", buf);
+					}
+	
+					if(strncmp(buf2, QUITMSG, strlen(QUITMSG)) == 0){
+						exit(1);
+					}
+				}	
+			
 				break;
-
-
+			case 2 : //game
+				
+				break;
+			
+	
 		}//switch
+
 	}//while
 
-	close(s1);
-	close(s2);
-	return 0;
 
+	close(s1);
+	close(s3);
+	close(s4);
+	return 0;
 }
